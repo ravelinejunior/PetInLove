d03b952960e2cb19ba76066409b8970d0652095a
ravelinejunior <franklin.junior@ctw.bmwgroup.com>
2022-08-31 15:54:57 +0100

    Saving stories

---
+++ app/src/main/java/com/raveline/petinlove/presentation/activities/ViewStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.raveline.petinlove.presentation.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.raveline.petinlove.R
+
+class ViewStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_story)
+    }
+}
